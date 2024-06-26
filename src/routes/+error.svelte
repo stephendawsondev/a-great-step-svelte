4bcda4ab3dbd89cb91b05da920ba97205868657b
Stephen Dawson <stephendawson@protonmail.com>
2024-06-26 18:31:49 +0100

    feat: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+	import ErrorPageSvg from '$lib/components/svgs/ErrorPageSvg.svelte';
+</script>
+
+<!-- Page not found page -->
+<section id="page-not-found-content" aria-labelledby="page-not-found-heading">
+	<div class="container">
+		<div class="image-container">
+			<ErrorPageSvg />
+		</div>
+		<div class="content-container">
+			<h1 id="page-not-found-heading">{$page.status}: {$page.error.message}</h1>
+			<a href="/" class="btn cta back-home" aria-label="Return to home page">Return home</a>
+		</div>
+	</div>
+</section>
